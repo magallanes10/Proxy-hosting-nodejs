76566bc1a7224075d180be26cbb7c84d22960a2b
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-08-31 10:33:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://freeuploaderfileccprojects.html-5.me' });
+  proxy.web(req, res, { target: 'http://node4.lunes.host:1120' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');

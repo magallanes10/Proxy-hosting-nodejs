22bbe161e345f6993b4615efeccde2e6dd5944b9
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-10-27 00:51:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://node4.lunes.host:1120' });
+  proxy.web(req, res, { target: 'http://jonellpogi.serv00.net' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');

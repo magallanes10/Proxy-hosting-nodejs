35755848b33c832338aa9c306e553219ca6da435
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-11-11 21:56:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://jonellpogi.serv00.net' });
+  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25622' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');

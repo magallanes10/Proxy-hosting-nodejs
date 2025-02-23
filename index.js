205b505107b7a7ae241a6e58971d0a2a53631da2
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2025-02-23 22:52:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://sgp1.hmvhostings.com:25750' });
+  proxy.web(req, res, { target: 'http://node01.inspirehost.fr:20775' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');
